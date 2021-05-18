77cebef44f9f3044c5227652d4f9306202ef851a
ALI Djamel <djamel.ali1920@gmail.com>
2021-05-18 02:00:32 +0200

    [UPDATE] update all test functions, and delete some others

---
+++ src/fun_print_n_times.c
@@ -0,0 +1,30 @@
+#include "include/fun_print_n_times.h"
+#include "include/lpc_types.h"
+#include "string.h"
+
+int print_n_times_str(void *params) {
+    int n, rc;
+    lpc_string *lpc_str = (lpc_string *)(params + sizeof(int));
+
+    size_t strlen_string = strlen(lpc_str->string) + 1;
+    memcpy(&n, params, sizeof(int));
+
+    if (n * strlen_string > lpc_str->slen) {
+        errno = ENOMEM;
+        lpc_str->slen = -1;
+        return -1;
+    }
+    char s[n * strlen_string];
+    char tmp_str[strlen_string + 1]; // +1 '\n'
+
+    rc = snprintf(s, strlen_string + 1, "%s\n", lpc_str->string);
+    strncpy(tmp_str, s, strlen(s));
+
+    for (int i = 1; i < n; i++) {
+        strcat(s, tmp_str);
+    }
+    memset(lpc_str->string, 0, lpc_str->slen);
+    memcpy(lpc_str->string, s, n * (strlen_string + 1));
+
+    return 0;
+}
---
+++ src/include/fun_print_n_times.h
@@ -0,0 +1,11 @@
+#ifndef PROG_SYSTEM_AV_PROJECT_FUN_PRINT_N_TIMES_H
+#define PROG_SYSTEM_AV_PROJECT_FUN_PRINT_N_TIMES_H
+/**
+ * @brief Affiche n fois la chaine de caractères contenue dans la structure lpc_string
+ * (Affichage une fois sur chque ligne)
+ *
+ * @return int (0 in success, else -1)
+*/
+ int print_n_times_str(void *params);
+
+#endif //PROG_SYSTEM_AV_PROJECT_FUN_PRINT_N_TIMES_H
