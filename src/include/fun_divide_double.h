383fd68be37277b8fbc463c996a3536776817d3d
ALI Djamel <djamel.ali1920@gmail.com>
2021-05-18 15:22:56 +0200

    [UPDATE] @brief of divide_double function

--- src/include/fun_divide_double.h
+++ src/include/fun_divide_double.h
@@ -1,7 +1,7 @@
 #ifndef PROG_SYSTEM_AV_PROJECT_FUN_DIVIDE_DOUBLE_H
 #define PROG_SYSTEM_AV_PROJECT_FUN_DIVIDE_DOUBLE_H
 
-/**@brief Prend en paramètre un double puis un entier, fais la division du double par l'entier et retourne
+/**@brief Prend en paramètre deux nombres de types double, fais la division du 1er par 2ème et retourne
  * le résultat dans le premier paramètre (qui est de type double)
  *
  * @return 0 si succès, -1 sinon*/
